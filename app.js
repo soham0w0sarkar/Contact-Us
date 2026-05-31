5a0b2f2cf47e98d2caf4fe32e08c99375a5a4f9e
soham0w0sarkar <sohamsarkar59@gmail.com>
2026-05-31 09:46:05 +0530

    INIT

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ app.use(json());
 
 const transporter = createTransport({
   host: "mail.privateemail.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
